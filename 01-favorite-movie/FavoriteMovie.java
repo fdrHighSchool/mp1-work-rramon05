3ae8ccbd7cc8ca061f532c6ac8a55d674425ce3c
AP Java Period 6 <apjavaperiod6@afvfx5003jc9h.78b505.nycboe.org>
2022-10-04 12:53:46 -0400

    added fav movie files

---
+++ 01-favorite-movie/FavoriteMovie.java
@@ -0,0 +1,19 @@
+
+/**
+ * Write a description of class FavoriteMovie here.
+ *
+ * @author (Randy Ramon)
+ * @version (9/22)
+ */
+public class FavoriteMovie{
+    public static void main(String[]args){
+        System.out.println("My Favorite Movie right now is Avengers End Game.");
+        System.out.print(" As the heros struggle to find Thanos, they also struggle o adapt to their new lives.");
+        System.out.print(" But then Scott appears at Avengers HQ with information that could help make things go back to normal.");
+        System.out.print(" As they pitch the idea to Tony, Tony doens't want to help beacause he fears to lose the family hehas created.");
+        System.out.print(" Tony fnds a picutre of Peter and in that moment he decided to help and begins working on Scotts idea. ");
+        System.out.print(" Will they be able to bring everyone back? Find out by watich Avengers Endgame on Disney+ right now!");
+  }
+}
+
+    
