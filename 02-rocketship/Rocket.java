2f48f77fbcdcab0a316711e2ed21142a060756b2
AP Java Period 6 <apjavaperiod6@afvfx5003jc9h.78b505.nycboe.org>
2022-10-04 12:55:15 -0400

    added rocketship files

---
+++ 02-rocketship/Rocket.java
@@ -0,0 +1,38 @@
+
+/**
+ * Write a description of class Rocket here.
+ *
+ * @author (Randy Ramon)
+ * @version (9/23/22)
+ */
+public class Rocket
+{
+    public static void main(String []args) {
+        top();
+        middle();
+        bottom();
+    }
+    
+    public static void top() {
+        System.out.println("   /\\");
+        System.out.println("  /  \\");
+        System.out.println(" /    \\");
+        System.out.println("+------+");
+        System.out.println("|      |");
+        System.out.println("|      |");
+        System.out.println("+------+");
+    }
+    public static void middle() {
+        System.out.println("|United|");
+        System.out.println("|States|");
+    }
+    public static void bottom() {
+        System.out.println("+------+");
+        System.out.println("|      |");
+        System.out.println("|      |");
+        System.out.println("+------+");
+        System.out.println("   /\\");
+        System.out.println("  /  \\");
+        System.out.println(" /    \\");
+    }
+}
