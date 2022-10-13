1a5ab4567e837e6e9a5ad7cf04f3071ae0326f58
AP Java Period 6 <apjavaperiod6@afvfx5003jc9h.78b505.nycboe.org>
2022-10-13 13:02:34 -0400

    fixed teach/sudent function

--- 06-username/UserName.java
+++ 06-username/UserName.java
@@ -1,42 +1,46 @@
- import java.util.Scanner;
+import java.util.Scanner;
 public class UserName {
-  public static void main(String[] args) {
-    // create Scanner object
-    Scanner s = new Scanner(System.in);
-
-    // get String input from user
-    System.out.print("Enter your first name: ");
-    String firstName = s.nextLine();
-    firstName = firstName.toLowerCase();
-    System.out.print("Enter your last name: ");
-    String lastName = s.nextLine();
-    lastName = lastName.toLowerCase();
-    System.out.print("What is your favorite number? ");
-    int favNum = s.nextInt();
-    System.out.print("Enter Student or Teacher: ");
-    String role = s.nextLine();
-    role = role.toLowerCase();
-    if (role.equals("teacher")){
-        System.out.println("Your new DOE username is "+ getInitial(firstName) + lastName + favNum + "@schools.nyc.gov");
-    }
-    else{
-       System.out.println("Your new DOE username is " + firstName + getInitial(lastName) + favNum + "@nycstudents.net"); 
-    }
-    // test output
+    public static void main(String[] args) {
+        // create Scanner object
+        Scanner s = new Scanner(System.in);
+        // get String input from user
+        System.out.print("Enter your first name: ");
+        String firstName = s.nextLine();
+        firstName = firstName.toLowerCase();
+        
+        System.out.print("Enter your last name: ");
+        String lastName = s.nextLine();
+        
+        System.out.print("Enter Student or Teacher: ");
+        String role = s.nextLine();
+        role = role.toLowerCase();
+        lastName = lastName.toLowerCase();
+        
+        System.out.print("What is your favorite number? ");
+        int favNum = s.nextInt();
+        
+        if (role.equals("teacher")){
+            System.out.println("Your new DOE username is "+ getInitial(firstName) + lastName + favNum + "@schools.nyc.gov");
+        }
+        else{
+           System.out.println("Your new DOE username is " + firstName + getInitial(lastName) + favNum + "@nycstudents.net"); 
+        }
+        // test output
+        
+        
+        
+        
+        s.close();
+    } // end main method
     
-    
-
-    s.close();
-  } // end main method
-
-  /*
-   * Name: initialize
-   * Purpose: send back the first character (inital) of a name
-   * Input: a name (String)
-   * Return: a single character (String)
-   */
-  public static String getInitial(String n) {
+    /*
+    * Name: initialize
+    * Purpose: send back the first character (inital) of a name
+    * Input: a name (String)
+    * Return: a single character (String)
+    */
+    public static String getInitial(String n) {
     return n.substring(0, 1);
-  } // end initialize method
+    } // end initialize method
 
 } // end class
