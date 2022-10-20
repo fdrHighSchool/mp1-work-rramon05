5e8d0fbdef94db7b3d0a723f7f1b74f9c67f561f
AP Java Period 6 <apjavaperiod6@afvfx5003jc9h.78b505.nycboe.org>
2022-10-20 12:46:00 -0400

    generatePassword works

--- 06-username/UserName.java
+++ 06-username/UserName.java
@@ -1,8 +1,9 @@
- import java.util.Scanner;
+import java.util.Scanner;
 public class UserName {
     public static void main(String[] args) {
         // create Scanner object
         Scanner s = new Scanner(System.in);
+        
         // get String input from user
         System.out.print("Enter your first name: ");
         String firstName = s.nextLine();
@@ -10,14 +11,18 @@ public class UserName {
         
         System.out.print("Enter your last name: ");
         String lastName = s.nextLine();
+        lastName = lastName.toLowerCase();
         
         System.out.print("Enter Student or Teacher: ");
         String role = s.nextLine();
         role = role.toLowerCase();
-        lastName = lastName.toLowerCase();
+        
         
         System.out.print("What is your favorite number? ");
         int favNum = s.nextInt();
+
+        
+        
         
         if (role.equals("teacher")){
             System.out.println("Your new DOE username is "+ getInitial(firstName) + lastName + favNum + "@schools.nyc.gov");
@@ -26,8 +31,7 @@ public class UserName {
            System.out.println("Your new DOE username is " + firstName + getInitial(lastName) + favNum + "@nycstudents.net"); 
         }
         // test output
-        
-        
+        generatePassword(8);
         
         
         s.close();
@@ -43,4 +47,34 @@ public class UserName {
     return n.substring(0, 1);
     } // end initialize method
 
+    /*
+    * Name: generatePassword
+    * Purpose: generte a random number
+    // from 65-90 CAPITAL leters
+    // from 97-122 Lowercase
+    // form 33-47 for Special Characters
+    
+    //Convert the random int to char, ex c = (char)i;
+    * Input: length
+    * Return: a random password
+    */
+    public static String generatePassword(int length){
+        String password ="" ;
+        int max = 90;
+        int min= 65;
+        int rand =((int)(Math.random() *((max - min) +1) + min) );
+        char c = (char)rand;
+            
+        
+        for(int i=0; i < length; i++){
+            rand =((int)(Math.random() *((max - min) +1) + min) );
+            c = (char)rand;
+            password += c;
+        }
+            
+        
+        
+        System.out.println(password);
+        return password;
+    }// End of generatePassword method
 } // end class
